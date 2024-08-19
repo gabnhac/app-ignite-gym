7a8eecaf90e7e6fc4150b2d4d800a813dd983717
unknown <gabriel.lucena@b2list.com>
2024-08-19 11:43:30 -0300

    feat: criação do cabeçalho do usuário

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,28 @@
+import { Heading, HStack, Text, VStack, Icon } from "@gluestack-ui/themed";
+import { UserPhoto } from "./UserPhoto";
+import { LogOut } from 'lucide-react-native';
+
+export function HomeHeader() {
+    return (
+        <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
+            <UserPhoto
+                source={{ uri: "https://github.com/gabnhac.png" }}
+                alt="Imagem usuário"
+                h="$16"
+                w="$16"
+
+            />
+
+            <VStack flex={1}>
+                <Text color="$gray100" fontSize="$sm">
+                    Olá,
+                </Text>
+                <Heading color="$gray100" fontSize="$md">
+                    Gabriel Lucena
+                </Heading>
+            </VStack>
+
+            <Icon as={LogOut} color="$gray200" size="xl"/>
+        </HStack>
+    )
+}
